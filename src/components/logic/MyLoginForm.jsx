ee9e981c17b90d5bd4fe7aed9447f1007859abfb
Kaappo Raivio <kaappo.raivio@gmail.com>
2020-08-08 15:23:36 +0300

    feat: add better login explanation
    Took 4 minutes

--- src/components/logic/MyLoginForm.jsx
+++ src/components/logic/MyLoginForm.jsx
@@ -37,7 +37,7 @@ class MyLoginForm extends Component {
 
 const schema = {
     title: "Log in",
-    explanation: "Please supply login credentials for a valid account on your SignalK server.",
+    explanation: "Please supply login credentials for a valid account on your SignalK server. These credentials are used for storing the settings of the app in the server. Your password will not be stored on the device. \n\n Hint: if you want to log in less often, you can increase the 'Login session timeout' parameter in the server's security settings.",
     fields: [
         {
             component: componentTypes.TEXT_FIELD,
