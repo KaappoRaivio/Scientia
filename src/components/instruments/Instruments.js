34a0f429184173619689dfee8b70c0102ad0aa7f
Kaappo Raivio <kaappo.raivio@gmail.com>
2020-08-11 11:55:34 +0300

    refactor: clean up Instruments.js and Instruments.css
    Took 2 minutes

--- src/components/instruments/Instruments.js
+++ src/components/instruments/Instruments.js
@@ -12,93 +12,90 @@ import TridataContainer from "./tridata/TridataContainer";
 import GaugeContainer from "./gauge/GaugeContainer";
 import VisualiserContainer from "./visualiser/VisualiserContainer";
 
-class Instruments extends React.Component {
-	render() {
-		const { animation, darkMode } = this.props.settings;
-		const { colors, instruments, onInstrumentAdded, onInstrumentRemoved, onInstrumentChanged, layoutEditingEnabled, signalkState } = this.props;
+const Instruments = props => {
+	const { colors, instruments, onInstrumentAdded, onInstrumentRemoved, onInstrumentChanged, layoutEditingEnabled, signalkState, settings } = props;
+	const { animation, darkMode } = settings;
 
-		return (
-			<div className="flexbox-container">
-				{instruments.map((instrument, index) => {
-					if (instrument.type === "single") {
-						const component = instrument.instruments[0];
+	return (
+		<div className="instrument-grid-container">
+			{instruments.map((instrument, index) => {
+				if (instrument.type === "single") {
+					const component = instrument.instruments[0];
 
-						return (
-							<SingleInstrumentContainer
-								animate={animation}
-								darkMode={darkMode}
-								colors={colors}
-								children={stringToClass(component.component)}
-								data={signalkState}
-								additionalProps={component.additionalProps}
-								resizeDebounce={0}
-								forceResize={true}
-								onRemoveClick={onInstrumentRemoved}
-								index={index}
-								layoutEditingEnabled={layoutEditingEnabled}
-							/>
-						);
-					} else if (instrument.type === "quadrant") {
-						return (
-							<QuadrantInstrumentContainer
-								layoutEditingEnabled={layoutEditingEnabled}
-								onInstrumentChanged={onInstrumentChanged}
-								index={index}
-								colors={colors}
-								animation={animation}
-								data={instrument.instruments}>
-								{[
-									...instrument.instruments.map((quadrant, innerIndex) => {
-										// console.log(quadrant, stringToClass(quadrant.component))
-										return (
-											<SingleInstrumentContainer
-												animate={animation}
-												darkMode={darkMode}
-												colors={colors}
-												children={stringToClass(quadrant.component)}
-												data={signalkState}
-												additionalProps={quadrant.additionalProps}
-												resizeDebounce={0}
-												forceResize={true}
-												onRemoveClick={_ => {
-													let newInstruments = instrument.instruments
-														.slice(0, innerIndex)
-														.concat(instrument.instruments.slice(innerIndex + 1));
+					return (
+						<SingleInstrumentContainer
+							animate={animation}
+							darkMode={darkMode}
+							colors={colors}
+							children={stringToClass(component.component)}
+							data={signalkState}
+							additionalProps={component.additionalProps}
+							resizeDebounce={0}
+							forceResize={true}
+							onRemoveClick={onInstrumentRemoved}
+							index={index}
+							layoutEditingEnabled={layoutEditingEnabled}
+						/>
+					);
+				} else if (instrument.type === "quadrant") {
+					return (
+						<QuadrantInstrumentContainer
+							layoutEditingEnabled={layoutEditingEnabled}
+							onInstrumentChanged={onInstrumentChanged}
+							index={index}
+							colors={colors}
+							animation={animation}
+							data={instrument.instruments}>
+							{[
+								...instrument.instruments.map((quadrant, innerIndex) => {
+									return (
+										<SingleInstrumentContainer
+											animate={animation}
+											darkMode={darkMode}
+											colors={colors}
+											children={stringToClass(quadrant.component)}
+											data={signalkState}
+											additionalProps={quadrant.additionalProps}
+											resizeDebounce={0}
+											forceResize={true}
+											onRemoveClick={_ => {
+												let newInstruments = instrument.instruments
+													.slice(0, innerIndex)
+													.concat(instrument.instruments.slice(innerIndex + 1));
 
-													onInstrumentChanged(index, {
-														type: "quadrant",
-														instruments: newInstruments,
-													});
-												}}
-												index={innerIndex}
-												layoutEditingEnabled={layoutEditingEnabled}
-											/>
-										);
-									}),
-								]}
-							</QuadrantInstrumentContainer>
-						);
-					} else {
-						return <div>Unknown instrument type {instrument.type}</div>;
-					}
-				})}
+												onInstrumentChanged(index, {
+													type: "quadrant",
+													instruments: newInstruments,
+												});
+											}}
+											index={innerIndex}
+											layoutEditingEnabled={layoutEditingEnabled}
+										/>
+									);
+								}),
+							]}
+						</QuadrantInstrumentContainer>
+					);
+				} else {
+					return <div>Unknown instrument type {instrument.type}</div>;
+				}
+			})}
 
-				{layoutEditingEnabled && (
-					<SingleInstrumentContainer
-						children={AddInstrument}
-						data={signalkState}
-						additionalProps={{ onInstrumentAdded }}
-						animate={animation}
-						darkMode={darkMode}
-						colors={colors}
-						forceResize={true}
-						layoutEditingEnabled={false}
-					/>
-				)}
-			</div>
-		);
-	}
-}
+			{layoutEditingEnabled && (
+				<SingleInstrumentContainer
+					children={AddInstrument}
+					data={signalkState}
+					additionalProps={{ onInstrumentAdded }}
+					animate={animation}
+					darkMode={darkMode}
+					colors={colors}
+					forceResize={true}
+					layoutEditingEnabled={false}
+				/>
+			)}
+		</div>
+	);
+};
 
 export const stringToClass = string =>
 	({
