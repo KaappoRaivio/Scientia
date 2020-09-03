a7cd26cd7cf4b5b9a27012e7dddd436e0585c636
Kaappo Raivio <kaappo.raivio@gmail.com>
2020-09-03 09:53:06 +0300

    chore: clean up repo
    Took 4 minutes

--- scripts/myPublish.sh
+++ scripts/myPublish.sh
@@ -7,7 +7,7 @@ yarn build
 cp -r build/ publish/public
 
 cp -r package.json node_modules publish/
-cp scientia-signalk-plugin/index.js publish/
+cp scripts/index.js publish/
 
 cd publish/ || exit
 npm publish
